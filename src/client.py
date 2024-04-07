d4628eaeac047ffab603f14b33c4ca7ccba5c18c
Ratul <ratul@cefalo.com>
2024-04-07 04:46:00 +0000

    typer invocation added

--- src/client.py
+++ src/client.py
@@ -1,8 +1,9 @@
 import grpc
 import my_service_pb2
 import my_service_pb2_grpc
+import typer
 
-def get_item(item_id):
+def get_item(item_id: str):
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = my_service_pb2_grpc.ItemServiceStub(channel)
         response = stub.GetItem(my_service_pb2.ItemId(id=item_id))
@@ -12,5 +13,4 @@ def get_item(item_id):
             print("Item not found.")
 
 if __name__ == '__main__':
-    # Test getting an item (Assume the item with id '1' does not exist yet)
-    get_item('1')
+    typer.run(get_item)
