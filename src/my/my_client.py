3df2547357bce7bd1ed1e5a78713b51ee118a888
Ratul <89194947+ratul-cefalo@users.noreply.github.com>
2024-05-08 10:43:03 +0000

    moving prev code to diff folder

---
+++ src/my/my_client.py
@@ -0,0 +1,16 @@
+import grpc
+import my_service_pb2
+import my_service_pb2_grpc
+import typer
+
+def get_item(item_id: str):
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = my_service_pb2_grpc.ItemServiceStub(channel)
+        response = stub.GetItem(my_service_pb2.ItemId(id=item_id))
+        if response.id:
+            print(f"Retrieved item: {response.name}")
+        else:
+            print("Item not found.")
+
+if __name__ == '__main__':
+    typer.run(get_item)
