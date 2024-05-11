59dd24438cdd99a4bd1d5fc9d47489d65c32be46
Ratul <89194947+ratul-cefalo@users.noreply.github.com>
2024-05-11 18:32:23 +0000

    added compression on both client and server

--- src/client.py
+++ src/client.py
@@ -4,7 +4,7 @@ import my_service_pb2_grpc
 import typer
 
 def get_item(item_id: str):
-    with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel('localhost:50051', compression=grpc.Compression.Deflate) as channel:
         stub = my_service_pb2_grpc.ItemServiceStub(channel)
         response = stub.GetItem(my_service_pb2.ItemId(id=item_id))
         if response.id:
--- src/server.py
+++ src/server.py
@@ -4,13 +4,15 @@ import sqlite3
 import my_service_pb2
 import my_service_pb2_grpc
 
+
 class ItemService(my_service_pb2_grpc.ItemServiceServicer):
     def __init__(self):
         self.conn = sqlite3.connect('items.db', check_same_thread=False)
         self.cursor = self.conn.cursor()
 
     def CreateItem(self, request, context):
-        self.cursor.execute("INSERT INTO items VALUES (?, ?)", (request.id, request.name))
+        self.cursor.execute("INSERT INTO items VALUES (?, ?)",
+                            (request.id, request.name))
         self.conn.commit()
         return request
 
@@ -22,7 +24,8 @@ class ItemService(my_service_pb2_grpc.ItemServiceServicer):
         return my_service_pb2.Item()
 
     def UpdateItem(self, request, context):
-        self.cursor.execute("UPDATE items SET name = ? WHERE id = ?", (request.name, request.id))
+        self.cursor.execute(
+            "UPDATE items SET name = ? WHERE id = ?", (request.name, request.id))
         self.conn.commit()
         return request
 
@@ -31,12 +34,16 @@ class ItemService(my_service_pb2_grpc.ItemServiceServicer):
         self.conn.commit()
         return my_service_pb2.Item()
 
+
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    my_service_pb2_grpc.add_ItemServiceServicer_to_server(ItemService(), server)
+    server = grpc.server(futures.ThreadPoolExecutor(
+        max_workers=10), compression=grpc.Compression.Deflate)
+    my_service_pb2_grpc.add_ItemServiceServicer_to_server(
+        ItemService(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
+
 
 if __name__ == '__main__':
     serve()
